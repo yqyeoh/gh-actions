6cb0d70c4e6dd4c726b2a46536868de870b9317d
yqyeoh <yqyeoh@hotmail.com>
2026-09-23 20:54:33 +0800

    update

--- main.tf
+++ main.tf
@@ -1,4 +1,13 @@
 terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+
   backend "s3" {
     bucket = "sctp-tfstate-ce13"
     key    = "yq/gh-actions-3-1"
